5495dba25b1f462630c6e316024d165149a3c835
Shannon Sos <ssos@pleasantonusd.net>
2024-10-29 10:43:41 -0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, W!");
-        System.out.println("DOES THIS WORK?");
-    }
+class App {
+  public static void main(String[] args) {
+    Book aBook = new Book();
+    System.out.println(aBook.pigLatin("hello"));
+    System.out.println(aBook.translateWord(""));
+  }
 }
-

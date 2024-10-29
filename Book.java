bcb9fd38407e8b2fa20b92d1814835c1470f51ca
Shannon Sos <ssos@pleasantonusd.net>
2024-10-29 09:57:15 -0700

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,34 @@
+//A few assumptions.......
+
+//Words will be separated by spaces. 
+//There can be punctuation in a word, we will only add/keep punctuation at the end of a string if it is at the end of a string.
+//    for examples: Hello.==> Ellohay.    Good-bye! ==> Ood-byegay!    so... ==> osay...
+
+public class Book
+{
+  public String pigLatin(String word)
+  {
+
+  }
+  
+  public int endPunctuation(String word)  //return the index of where the punctuation is at the end of a String. If it is all punctuation return 0, if there is no punctuation return -1
+  {
+
+    return -1;
+  }
+
+  public String translateWord(String word)    //to share with class
+  {
+    String convertedWord = "";
+
+    return convertedWord;
+  }
+
+  public String translateSentence(String sentence)
+  {
+    String retSentence = "";
+
+
+    return retSentence;
+  }
+}  

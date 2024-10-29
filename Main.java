4dc10e27199c4be1037128bbc9db24caf95e8781
Shannon Sos <ssos@pleasantonusd.net>
2024-10-29 10:53:00 -0700

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    Book aBook = new Book();
-    System.out.println(aBook.pigLatin("hello"));
-    System.out.println(aBook.translateWord(""));
-  }
-}
